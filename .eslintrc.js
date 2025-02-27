00be7b1ed6d63c3d212c08fc01bf526bd35494cd
choi-ik <ci980704@naver.com>
2025-02-27 20:08:56 +0900

    chore: 린팅 rule 설정

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project,
+  },
+  plugins: ["import", "unused-imports", "react-refresh"],
+  rules: {
+    "unused-imports/no-unused-imports": "error",
+    "@typescript-eslint/no-unused-vars": "error",
+    "no-var": "error",
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", prev: "*", next: "return" },
+    ],
+    "space-before-blocks": [2, "always"],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "react/function-component-definition": ["error", { namedComponents: ["function-declaration"] }],
+    "react/jsx-key": "error",
+    "react/jsx-fragments": ["error", "syntax"],
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "index",
+          "type",
+          "unknown",
+        ],
+        pathGroups: [
+            { pattern: "**/{assets,components,constants,hooks,types,ui,utils}/**", group: "internal", position: "after" }
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: ["node_modules/", "dist/"],
+}
