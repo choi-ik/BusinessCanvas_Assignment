1fc9ca4197af05e7f5174e124b8fa004674857d4
choi-ik <ci980704@naver.com>
2025-03-01 19:49:20 +0900

    refactor: 모달 Close 컴포넌트 핸들러 가독성 개선

--- src/ui/Modal/Close.tsx
+++ src/ui/Modal/Close.tsx
@@ -9,11 +9,15 @@ const Close = forwardRef<HTMLButtonElement, ComponentProps<"button">>(
   ({ className, ...props }, ref) => {
     const { setOpen } = useModalContext();
 
+    const handleClickClose = () => {
+      setOpen?.(false);
+    };
+
     return (
       <button
         ref={ref}
         type="button"
-        onClick={() => setOpen?.(false)}
+        onClick={handleClickClose}
         className={cn("opacity-70 transition-opacity hover:opacity-100", className)}
         {...props}
       >
