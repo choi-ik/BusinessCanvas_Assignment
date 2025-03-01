204ca7f0090ed33db681dafbcf63f9d587d78418
choi-ik <ci980704@naver.com>
2025-03-02 03:20:21 +0900

    feat: main 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import FilterBar from "./components/FilterBar";
+import Record from "./components/Record";
+import RecordForm from "./components/RecordForm";
+import { DEFAULT_RECORDS } from "./constants/record";
+import { MEMBER_KEY } from "./constants/storageKey";
+import { useArrayStorage } from "./hooks/useArrayStorage";
+import { MemberRecord } from "./types/record";
+import { Button } from "./ui/Button";
+import { Icon } from "./ui/Icon";
+import { Modal } from "./ui/Modal";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    value: records,
+    add,
+    remove,
+    updateItem,
+    setValue,
+  } = useArrayStorage<MemberRecord>(MEMBER_KEY, DEFAULT_RECORDS);
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-screen w-full justify-center p-3">
+      <div className="w-[81.063rem]">
+        <div className="flex justify-end px-[0.875rem] py-2">
+          <Modal open={modalIsOpen} onOpenChange={setModalIsOpen}>
+            <Modal.Trigger asChild>
+              <Button>
+                <Icon name="Plus" size={16} />
+                <span className="text-sm">추가</span>
+              </Button>
+            </Modal.Trigger>
+            <Modal.Content>
+              <RecordForm onOpenChange={setModalIsOpen} addRecord={add} />
+            </Modal.Content>
+          </Modal>
+        </div>
+        <FilterBar setRecords={setValue} />
+        {records?.map(({ name, address, memo, joinDate, job, emailConsent }) => (
+          <Record
+            key={name}
+            name={name}
+            address={address}
+            memo={memo}
+            joinDate={joinDate}
+            job={job}
+            emailConsent={emailConsent}
+            removeRecord={remove}
+            updateRecord={updateItem}
+          />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
